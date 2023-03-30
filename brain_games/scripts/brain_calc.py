7d8cad78eb81137d6bc2c2fe20cdf0bedc9194f8
aranto <aranto521@yandex.ru>
2023-03-30 20:42:04 +0300

    .

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,26 @@
+import random
+import prompt
+from brain_games.cli import name 
+
+
+def logic_calc():
+    operators = ['+', '-', '*']
+    counter = 0
+    while counter < 3:
+        First_number = random.randint(0, 100)
+        Second_number = random.randint(0, 100)
+        Random_operator = random.choice(operators)
+        print("What is the result of the expression?")
+        print("Question:", First_number + Random_operator + Second_number)
+        correct_answer = eval(First_number + Random_operator + Second_number)
+        answer = prompt.string("Your answer: ")
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(answer + " is wrong answer ;(. Correct answer was " + correct_answer + "\nLets's try again, " + name + "!")
+            break
+        counter += 1
+    if counter == 3:
+        print("Congratulations, " + name + "!")
+        
+    
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_even import logic_even
+from brain_games.scripts.brain_calc import logic_calc
 
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
     logic_even()
+    logic_calc()
     
 
 
