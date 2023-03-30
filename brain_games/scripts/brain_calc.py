249b1734f2c4053ec94fd87ea106c902cfc85b9f
aranto <aranto521@yandex.ru>
2023-03-30 22:27:20 +0300

    .

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,23 +4,24 @@ from brain_games.cli import name
 
 
 def logic_calc():
-    operators = ['+', '-', '*']
+    operators = ["+", "-", "*"]
     counter = 0
     while counter < 3:
         First_number = random.randint(0, 100)
         Second_number = random.randint(0, 100)
         Random_operator = random.choice(operators)
         print("What is the result of the expression?")
-        print("Question:", First_number, Random_operator, Second_number)
-        correct_answer = eval(First_number, Random_operator, Second_number)
+        print("Question:", int(First_number), Random_operator, int(Second_number))
         answer = prompt.string("Your answer: ")
+        correct_answer = eval(" int(First_number) " + Random_operator + " int(Second_number)")
         if answer == correct_answer:
             print("Correct!")
         else:
-            print(answer + " is wrong answer ;(. Correct answer was " + correct_answer + "\nLets's try again, " + name + "!")
+            print("'" + str(answer) + "'" + " is wrong answer ;(. Correct answer was " + "'" + str(correct_answer) + "'" + "\nLet's try again, " + name + "!")
             break
         counter += 1
     if counter == 3:
         print("Congratulations, " + name + "!")
         
     
+ 
